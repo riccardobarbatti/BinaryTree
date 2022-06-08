c5b79f313155af1de7d1bfaa00ad9bee228252c3
Riccardo <>
2022-06-08 13:23:43 +0200

    Add Readme Demo

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BinaryTree
-Binary Tree JS https://en.wikipedia.org/wiki/Binary_search_tree
+Binary Tree JS
+
+https://en.wikipedia.org/wiki/Binary_search_tree
+
+## Demo
+![Demo](bsc.jpg)
+<br />
+
+- Insert Node
+- Delete Node
+- Search Node
+- Convert JSON Format
+
--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 const {BinaryTree} = require('./src/BinaryTree.js');
 const {traverse} = require("./src/BinaryTree");
 //-------- Full Binary Tree ------------
-//               9
+//               10
 //            /     \
 //           4      20
 //         /  \    /  \
-//        1    6  15  80
+//        2    6  15  80
 //---------------------------------------
 //Tree Object
 const tree = new BinaryTree();
-tree.insert(9);
+tree.insert(10);
 tree.insert(4);
 tree.insert(6);
 tree.insert(20);
 tree.insert(80);
 tree.insert(15);
-tree.insert(1);
+tree.insert(2);
 //Seach node lookUp
 //tree.lookup(20);
 //Delete Node
